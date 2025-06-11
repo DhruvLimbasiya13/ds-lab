4c845fb7357ac3748ef08b2378a63b838b99aa63
Dhruv Limbasiya <24031701027@darshan.ac.in>
2025-06-11 10:51:51 +0530

    new program

--- lab 3/A4_19.c
+++ lab 3/A4_19.c
@@ -1,4 +1,34 @@
 #include<stdio.h>
-void main(){
-    
+void main()
+{
+    int i,n,max,min;
+    printf("enter how many array you want=");
+    scanf("%d",&n);
+
+    int a[n];
+    for (i=0;i<n;i++)
+    {
+        printf("enter a number=");
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    for (i=0;i<n;i++)
+    {
+        if (a[i]>max)
+        {
+            max=a[i];
+        }
+        
+    }
+    min=a[0];
+    for (i=0;i<n;i++)
+    {
+        if (a[i]<min)
+        {
+            min=a[i];
+        }
+        
+    }
+    printf("max=%d\n",max);
+    printf("min=%d",min);
 }
