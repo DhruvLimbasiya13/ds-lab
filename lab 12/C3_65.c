71f046146e73d61f61420c9fe54a94586b80c56d
Dhruv Limbasiya <24031701027@darshan.ac.in>
2025-07-05 18:59:21 +0530

    new program and modified

---
+++ lab 12/C3_65.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int info;
+    struct node *link;
+};
+
+struct node *first = NULL;
+int count = 0;
+
+void InsertAtFirst()
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    int x;
+    printf("\nenter the info of new node.\n");
+    scanf("%d", &x);
+    newNode->info = x;
+
+    if (first == NULL)
+    {
+        first = newNode;
+        newNode->link = NULL;
+    } // if
+    else
+    {
+        newNode->link = first;
+        first = newNode;
+    } // else
+
+    printf("Info of first Node : %d\n", first->info);
+    count++;
+} // insert at first
+
+void InsertAtLast()
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    int x;
+    printf("\nenter the info of new node.\n");
+    scanf("%d", &x);
+    newNode->info = x;
+
+    if (first == NULL)
+    {
+        newNode->link = first;
+        first = newNode;
+
+    } // if
+    else
+    {
+        struct node *save = first;
+        while (save->link != NULL)
+        {
+            save = save->link;
+        } // while
+
+        save->link = newNode;
+        newNode->link = NULL;
+
+    } // else
+
+    printf("Info of last Node : %d\n", newNode->info);
+    count++;
+} // insert at last
+
+void DisplayAllNodes()
+{
+    if (first == NULL)
+    {
+        printf("Nodes doesn't exist .\n");
+    } // if
+    else
+    {
+        printf("\n");
+        struct node *save = first;
+        int i = 1;
+        while (save != NULL)
+        {
+            printf("value of Node %d : %d\n", i, save->info);
+            save = save->link;
+            i++;
+        } // while
+    } // else
+} // display node
+
+void replaceNode()
+{
+    if (count == 0)
+    {
+        printf("\nLinked list is empty.\n");
+        return;
+    } // if
+
+    printf("\nEnter the position (k) to replace from both sides:\n");
+    int k;
+    scanf("%d", &k);
+
+    if (k <= 0 || k > count)
+    {
+        printf("Invalid position. It must be between 1 and %d.\n", count);
+        return;
+    } // if
+
+    int fromFront = k;
+    int fromEnd = count - k + 1;
+
+    struct node *front = first;
+    for (int i = 1; i < fromFront; i++)
+    {
+        front = front->link;
+    } // for
+
+    struct node *end = first;
+    for (int i = 1; i < fromEnd; i++)
+    {
+        end = end->link;
+    } // for
+
+    int temp = front->info;
+    front->info = end->info;
+    end->info = temp;
+    printf("Replacement completed.\n");
+} // replace the nodes
+
+void main()
+{
+    int choice;
+    while (1)
+    {
+        printf("\n1.Insert At first.");
+        printf("\n2.Insert At last.");
+        printf("\n3.Replace the Nodes.");
+        printf("\n4.Display.");
+        printf("\n5.Exit.");
+        printf("\nEnter Choice : ");
+        int x;
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            InsertAtFirst();
+            break;
+        case 2:
+            InsertAtLast();
+            break;
+        case 3:
+            replaceNode();
+            break;
+        case 4:
+            DisplayAllNodes();
+            break;
+        case 5:
+            exit(0);
+            break;
+        default:
+            printf("\ninvalid choice.\n");
+            break;
+        }
+    }
+}
---
+++ lab 13/C3_68.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int info;
+    struct node *link;
+};
+
+struct node *first = NULL;
+
+void InsertAtFirst(int x)
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+
+    newNode->info = x;
+
+    if (first == NULL)
+    {
+        first = newNode;
+        newNode->link = NULL;
+    } // if
+    else
+    {
+        newNode->link = first;
+        first = newNode;
+    } // else
+
+    printf("Info of first Node : %d\n", first->info);
+
+} // insert at first
+
+void InsertAtLast(int x)
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+
+    newNode->info = x;
+
+    if (first == NULL)
+    {
+        newNode->link = first;
+        first = newNode;
+
+    } // if
+    else
+    {
+        struct node *save = first;
+        while (save->link != NULL)
+        {
+            save = save->link;
+        } // while
+
+        save->link = newNode;
+        newNode->link = NULL;
+
+    } // else
+} // insert at last
+
+void sortNodes()
+{
+    if (first == NULL || first->link == NULL)
+    {
+        printf("List is empty or has only one node.\n");
+        return;
+    }//if
+
+    struct node *end = NULL;
+
+    while (end != first->link)
+    {
+        struct node *pred = NULL;
+        struct node *save = first;
+
+        while (save->link != end)
+        {
+            struct node *next = save->link;
+
+            if (save->info > next->info)
+            {
+                // Swap links using temp
+                struct node *temp = next->link;
+
+                if (pred == NULL)
+                {
+                    // Swapping at the beginning of the list
+                    next->link = save;
+                    save->link = temp;
+                    first = next;
+                    pred = next;
+                }//inner if
+                else
+                {
+                    pred->link = next;
+                    save->link = temp;
+                    next->link = save;
+                    pred = next;
+                }//inner if
+            }//outer if
+            else
+            {
+                // Move pointers ahead
+                pred = save;
+                save = save->link;
+            }//outer else
+        }//inner while
+
+        end = save;
+    }//while
+
+    printf("\nSorted successfully.\n");
+}//sort
+
+
+void DisplayAllNodes()
+{
+    if (first == NULL)
+    {
+        printf("Nodes doesn't exist .\n");
+    } // if
+    else
+    {
+        struct node *save = first;
+        int i = 1;
+        while (save != NULL)
+        {
+            printf("value of Node %d : %d\n", i, save->info);
+            save = save->link;
+            i++;
+        } // while
+    } // else
+} // dislpay nodes
+
+void main()
+{
+    while (1)
+    {
+        printf("\n1.Insert a node at the front of the linked list. \n2.Display all nodes.\n3.Insert a node at the end of the linked list.\n4.Sort the link list .\n5. exit\n Enter your Choice\n");
+        int choice;
+        scanf("%d", &choice);
+        int x;
+
+        switch (choice)
+        {
+        case 1:
+            printf("enter the info of new Node :\n");
+
+            scanf("%d", &x);
+            InsertAtFirst(x);
+            break;
+
+        case 2:
+            DisplayAllNodes();
+            break;
+
+        case 3:
+            printf("enter the info of new Node :\n");
+            scanf("%d", &x);
+            InsertAtLast(x);
+            break;
+
+        case 4:
+            sortNodes();
+            break;
+
+        case 5:
+            exit(0);
+        default:
+            break;
+        } // switch
+    } // while
+} // main
